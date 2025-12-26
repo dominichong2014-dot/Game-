919e913a69b2a0dd6694f2f4efab50b73c4f9c7a
dominichong2014-dot <dominichong2014@gmail.com>
2025-12-26 19:44:38 +0900

    Made code
    code

--- Game.py
+++ Game.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+
+#1. 페이즈설정 (왭사이트 제목)
+st.set_page_config(page_title='우리들에 공유 달릭', layout="centered
+
+st.titile("전시회 공유 달력 & 방멱록")
+st.write("원하는 날짜에 일정을 남겨보세요. 모든 사람과 공유됍니다!")
